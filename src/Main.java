b0674fdac76b7ba90b6cdb76fd09ec4e26c91a21
kttsu <150462533+kttsu@users.noreply.github.com>
2023-11-18 00:10:14 +0900

    Initial commit
    1から10をHello World!、11から20まで Hello! と表示するようにしました。

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if(i > 10 ) {
+            System.out.println("Hello!");
+            } else {
+            System.out.println("Hello World!");
+            }
+        }
+    }
+}
